e75344b2f27cdbc4b1dccfa7b93c855695132741
Luke <lukegriggs100@gmail.com>
2024-06-13 12:37:33 -0500

    landing page rough skeleton

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="flex h-full items-center font-semibold bg-[#11909E] px-10 py-5">
+        <div className="flex w-full">
+          <a href="/">
+            <span className="mr-6 flex cursor-pointer text-white flex-row text-3xl text-input-foreground">
+              Birdfeed
+            </span>
+          </a>
+        </div>
+        <div className="w-full">
+          <ul className="flex space-x-8 justify-center">
+            <li>
+              <a href="#" className="text-white">
+                identify
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-white">
+                Seeds
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-white">
+                Support
+              </a>
+            </li>
+          </ul>
+        </div>
+        {/* Desktop View */}
+        <div className="hidden w-full justify-end lg:flex">
+          <ul className="text-l ml-auto mr-0 flex items-center font-medium text-gray-100">
+            <li>
+              <a href="/" className="mr-4">
+                login
+              </a>
+            </li>
+            <li>
+              <a href="/demo">
+                <div className="bg-amber-300 hover:bg-amber-400 rounded-[95px] px-3 py-2 text-sm">
+                  sign up
+                </div>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
